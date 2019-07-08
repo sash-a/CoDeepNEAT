69404397741b7f3cbb7232cc045749da0b18e03a
Sasha Abramowitz <reallysasha@gmail.com>
2019-07-08 22:13:31 +0200

    Changes to file logging and fixed unpacking error in generation

--- src/Analysis/RuntimeAnalysis.py
+++ src/Analysis/RuntimeAnalysis.py
@@ -2,42 +2,41 @@ from src.Analysis.GenerationData import GenerationData
 import inspect, os
 import os.path
 
-
 generations = []
 log_file = None
 
 
-
-def log_new_generation(accuracies, generation_number = None):
+def log_new_generation(accuracies, generation_number=None):
     global generations
     global log_file
     if generation_number is None:
-       generation_number = len(generations)
+        generation_number = len(generations)
 
     generations.append(GenerationData(accuracies, generation_number))
-    f = open(get_log_folder() + log_file, "a+")
-    f.write(generations[-1].get_summary()+"\n")
-    f.close()
+
+    with open(get_log_folder() + log_file, "a+") as f:
+        f.write(generations[-1].get_summary() + "\n")
 
 
-def get_next_log_file_name(log_file_name = None):
+def get_next_log_file_name(log_file_name=None):
     if log_file_name is None:
         log_file_name = "log"
 
     file_exists_already = os.path.isfile(get_log_folder() + log_file_name + ".txt")
-    if(file_exists_already):
+    if (file_exists_already):
         counter = 1
-        while(file_exists_already):
-            file_exists_already = os.path.isfile(get_log_folder()  + log_file_name +"_"+repr(counter)+ ".txt")
-            counter+=1
-        counter-=1
-        log_file_name = log_file_name +"_"+repr(counter)
-    return log_file_name +".txt"
+        while (file_exists_already):
+            file_exists_already = os.path.isfile(get_log_folder() + log_file_name + "_" + repr(counter) + ".txt")
+            counter += 1
+        counter -= 1
+        log_file_name = log_file_name + "_" + repr(counter)
+    return log_file_name + ".txt"
 
 
-def configure(log_file_name = None):
+def configure(log_file_name=None):
     global log_file
     log_file = get_next_log_file_name(log_file_name)
+
 
 def get_log_folder():
     return os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + "\\Logs\\"
--- src/EvolutionEnvironment/Generation.py
+++ src/EvolutionEnvironment/Generation.py
@@ -35,7 +35,6 @@ class Generation:
                                                         Props.BP_CONN_MUTATION_CHANCE,
                                                         Props.BP_TARGET_NUM_SPECIES)
 
-
         print('population initialized')
         return module_population, blueprint_population
 
@@ -67,35 +66,36 @@ class Generation:
 
             if Config.protect_parsing_from_errors:
                 try:
-                    acc, module_graph = self.evaluate_blueprints(blueprint_individual , inputs,generation_number)
+                    acc, module_graph, _ = self.evaluate_blueprints(blueprint_individual, inputs, generation_number)
                 except:
                     print("blueprint indv ran with errors")
                     continue
             else:
-                acc, module_graph, blueprint_individual = self.evaluate_blueprints(blueprint_individual, inputs, generation_number)
+                acc, module_graph, blueprint_individual = self.evaluate_blueprints(blueprint_individual, inputs,
+                                                                                   generation_number)
 
             if acc >= best_acc:
                 best_acc = acc
                 best_bp = module_graph
             accuracies.append(acc)
 
-        if(generation_number % 1 == 0):
-            if(Config.print_graphs):
-                best_bp.plot_tree(title="gen:"+str(generation_number)+" acc:"+str(best_acc))
+        if (generation_number % 1 == 0):
+            if (Config.print_graphs):
+                best_bp.plot_tree(title="gen:" + str(generation_number) + " acc:" + str(best_acc))
 
-        RuntimeAnalysis.log_new_generation(accuracies,generation_number)
+        RuntimeAnalysis.log_new_generation(accuracies, generation_number)
         print('\n\nbest acc', best_acc)
 
     def evaluate_blueprints(self, blueprint_individual, inputs, generation_number):
 
         blueprint = blueprint_individual.to_blueprint()
         module_graph = blueprint.parseto_module_graph(self)
-        if(module_graph is None):
+        if (module_graph is None):
             raise Exception("null module graph produced from blueprint")
 
         # net = module_graph.to_nn(in_features=1, device=device)
         try:
-            #print("using infeatures = ",module_graph.get_first_feature_count(inputs))
+            # print("using infeatures = ",module_graph.get_first_feature_count(inputs))
             net = module_graph.to_nn(in_features=module_graph.get_first_feature_count(inputs))
         except Exception as e:
             print("Error:", e)
@@ -116,11 +116,9 @@ class Generation:
 
         net_size = net.module_graph.get_net_size()
 
-        blueprint_individual.report_fitness(acc,net_size)
+        blueprint_individual.report_fitness(acc, net_size)
 
         for module_individual in blueprint_individual.modules_used:
-            module_individual.report_fitness(acc,net_size)
-
-
+            module_individual.report_fitness(acc, net_size)
 
         return acc, module_graph, blueprint_individual
