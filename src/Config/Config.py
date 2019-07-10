4ad271ee7f58f7e8f4a0f827c57ef504294ec92b
Sasha Abramowitz <reallysasha@gmail.com>
2019-07-10 14:03:12 +0200

    Updated config for CHPC

--- src/Config/Config.py
+++ src/Config/Config.py
@@ -3,20 +3,15 @@ import torch
 # device = torch.device('cuda:0')
 device = torch.device('cpu')
 
-
-second_objective = "network_size"#network_size|""
+second_objective = "network_size"  # network_size|""
 third_objective = ""
 
-
 test_in_run = False
-dummy_run = True
+dummy_run = False
 protect_parsing_from_errors = True
-
 
 number_of_epochs_per_evaluation = 3
 
-
-print_best_graphs=False
+print_best_graphs = False
 print_best_graph_every_n_generations = 5
-print_failed_graphs = True
-
+print_failed_graphs = False
