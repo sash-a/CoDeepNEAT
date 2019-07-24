719cd25cf033d94d761ab2ef84cb3a1560fdfb49
Sasha Abramowitz <reallysasha@gmail.com>
2019-07-24 20:05:12 +0200

    Fixed the daemon spawning child processes error

--- src/Config/Config.py
+++ src/Config/Config.py
@@ -2,25 +2,26 @@ import torch
 import operator
 import torch.multiprocessing as mp
 
+# --------------------------------------------------------------------------------------------------------------------#
+# Run state options
 run_name = "test_run"
 continue_from_last_run = False
 
-
-
+# --------------------------------------------------------------------------------------------------------------------#
+# nn options
 device = 'gpu'
-#device = 'cpu'
+# device = 'cpu'
 num_gpus = 2
-num_workers = 2
+num_workers = 0  # TODO this doesn't work in parallel because daemonic processes cannot spawn children
 dataset = 'mnist'
 data_path = ''
-
-
-
-max_num_generations = 150
 number_of_epochs_per_evaluation = 3
 
+# --------------------------------------------------------------------------------------------------------------------#
+max_num_generations = 150
 
-
+# --------------------------------------------------------------------------------------------------------------------#
+# Multiobjective options
 second_objective = "network_size"  # network_size
 second_objective_comparator = operator.lt  # lt for minimisation, gt for maximisation
 third_objective = ""
@@ -28,22 +29,25 @@ third_objective_comparator = operator.lt
 
 moo_optimiser = "cdn"  # cdn/nsga
 
-
-
+# --------------------------------------------------------------------------------------------------------------------#
+# Data augmentation options
 evolve_data_augmentations = True
 
-
-
-test_in_run = False
+# --------------------------------------------------------------------------------------------------------------------#
+# Debug and test options
 dummy_run = False
+
 protect_parsing_from_errors = False
+test_in_run = False
+interleaving_check = False
+
 save_best_graphs = True
 print_best_graphs = False
 print_best_graph_every_n_generations = 5
 save_failed_graphs = True
 
-interleaving_check = True
 
+# --------------------------------------------------------------------------------------------------------------------#
 
 def get_device():
     """Used to obtain the correct device taking into account multiple gpus"""
