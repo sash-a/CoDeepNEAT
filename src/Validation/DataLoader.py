9521fb41d5ad9c0df97c5d97c8dbbf7109e6b7cc
shane <shaneacton745.sa@gmail.com>
2019-07-28 16:16:15 +0200

    .

--- src/Validation/DataLoader.py
+++ src/Validation/DataLoader.py
@@ -20,7 +20,7 @@ def load_data(batch_size=64, dataset=""):
         transforms.Normalize((0.5,), (0.5,))
     ])
 
-    download = False
+    download = True
 
     if dataset == "":
         dataset = Config.dataset.lower()
