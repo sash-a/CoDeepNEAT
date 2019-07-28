d7ff711386f2d7b49b745c3f1477f7266c75ecc5
shane <shaneacton745.sa@gmail.com>
2019-07-28 14:09:41 +0200

    .

--- src/CoDeepNEAT/CDNGenomes.py
+++ src/CoDeepNEAT/CDNGenomes.py
@@ -98,7 +98,7 @@ class DAGenome(Genome):
         return True
 
     def mutate(self, mutation_record):
-        print("mutating DA genome")
+        #print("mutating DA genome")
         return super()._mutate(mutation_record, 0.1, 0, allow_connections_to_mutate=False, debug=False)
 
     def to_phenotype(self, Phenotype=None):
