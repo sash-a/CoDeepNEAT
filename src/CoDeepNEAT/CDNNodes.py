b6a786b17a95375b4ae857fedb2efd3b73896947
Doggus <ldoggy20@gmail.com>
2019-08-14 15:35:31 +0200

    Upped mutation chances for DA submutagens

--- src/CoDeepNEAT/CDNNodes.py
+++ src/CoDeepNEAT/CDNNodes.py
@@ -150,70 +150,70 @@ class DANode(NodeGene):
 
             "Rotate": {
                 "lo": Mutagen(value_type=ValueType.WHOLE_NUMBERS, current_value=-30, start_range=-180,
-                              end_range=-1),
+                              end_range=-1, mutation_chance=0.2),
                 "hi": Mutagen(value_type=ValueType.WHOLE_NUMBERS, current_value=30, start_range=0,
-                              end_range=180)},
+                              end_range=180, mutation_chance=0.2)},
 
             "Translate_Pixels": {
                 "x_lo": Mutagen(value_type=ValueType.WHOLE_NUMBERS, current_value=-4, start_range=-15,
-                                end_range=-1),
+                                end_range=-1, mutation_chance=0.2),
                 "x_hi": Mutagen(value_type=ValueType.WHOLE_NUMBERS, current_value=4, start_range=0,
-                                end_range=15),
+                                end_range=15, mutation_chance=0.2),
                 "y_lo": Mutagen(value_type=ValueType.WHOLE_NUMBERS, current_value=-4, start_range=-15,
-                                end_range=-1),
+                                end_range=-1, mutation_chance=0.2),
                 "y_hi": Mutagen(value_type=ValueType.WHOLE_NUMBERS, current_value=4, start_range=0,
-                                end_range=15)},
+                                end_range=15, mutation_chance=0.2)},
 
             "Scale": {
                 "x_lo": Mutagen(value_type=ValueType.CONTINUOUS, current_value=0.75, start_range=0.25,
-                                end_range=0.99),
+                                end_range=0.99, mutation_chance=0.3),
                 "x_hi": Mutagen(value_type=ValueType.CONTINUOUS, current_value=1.25, start_range=1.0,
-                                end_range=2.0),
+                                end_range=2.0, mutation_chance=0.3),
                 "y_lo": Mutagen(value_type=ValueType.CONTINUOUS, current_value=0.75, start_range=0.25,
-                                end_range=0.99),
+                                end_range=0.99, mutation_chance=0.3),
                 "y_hi": Mutagen(value_type=ValueType.CONTINUOUS, current_value=1.25, start_range=1.0,
-                                end_range=2.0)},
+                                end_range=2.0, mutation_chance=0.3)},
 
             "Pad_Pixels": {
                 "lo": Mutagen(value_type=ValueType.WHOLE_NUMBERS, current_value=2, start_range=0,
-                              end_range=3),
+                              end_range=3, mutation_chance=0.2),
                 "hi": Mutagen(value_type=ValueType.WHOLE_NUMBERS, current_value=4, start_range=4,
-                              end_range=8),
-                "s_i": Mutagen(True, False, discreet_value=False, mutation_chance=0.25)},
+                              end_range=8, mutation_chance=0.2),
+                "s_i": Mutagen(True, False, discreet_value=False, mutation_chance=0.2)},
 
             "Crop_Pixels": {
                 "lo": Mutagen(value_type=ValueType.WHOLE_NUMBERS, current_value=2, start_range=0,
-                              end_range=3),
+                              end_range=3, mutation_chance=0.2),
                 "hi": Mutagen(value_type=ValueType.WHOLE_NUMBERS, current_value=4, start_range=4,
-                              end_range=8),
-                "s_i": Mutagen(True, False, discreet_value=False, mutation_chance=0.25)},
+                              end_range=8, mutation_chance=0.2),
+                "s_i": Mutagen(True, False, discreet_value=False, mutation_chance=0.2)},
 
             "Custom_Canny_Edges": {
                 "min_val": Mutagen(value_type=ValueType.WHOLE_NUMBERS, current_value=100, start_range=50,
-                                   end_range=149),
+                                   end_range=149, mutation_chance=0.2),
                 "max_val": Mutagen(value_type=ValueType.WHOLE_NUMBERS, current_value=200, start_range=150,
-                                   end_range=250)},
+                                   end_range=250, mutation_chance=0.2)},
 
             "Additive_Gaussian_Noise": {
                 "lo": Mutagen(value_type=ValueType.CONTINUOUS, current_value=0.05, start_range=0.0,
-                              end_range=0.09),
-                "hi": Mutagen(value_type=ValueType.CONTINUOUS, current_value=0.1, start_range=0.10,
-                              end_range=0.40),
+                              end_range=0.09, mutation_chance=0.3),
+                "hi": Mutagen(value_type=ValueType.CONTINUOUS, current_value=0.15, start_range=0.10,
+                              end_range=0.30, mutation_chance=0.3),
                 "percent": Mutagen(value_type=ValueType.CONTINUOUS, current_value=0.6, start_range=0.2,
-                                   end_range=0.8)
+                                   end_range=0.8, mutation_chance=0.3)
             },
 
             "Coarse_Dropout": {
                 "d_lo": Mutagen(value_type=ValueType.CONTINUOUS, current_value=0.05, start_range=0.0,
-                                end_range=0.09),
+                                end_range=0.09, mutation_chance=0.3),
                 "d_hi": Mutagen(value_type=ValueType.CONTINUOUS, current_value=0.2, start_range=0.1,
-                                end_range=0.3),
+                                end_range=0.3, mutation_chance=0.3),
                 "s_lo": Mutagen(value_type=ValueType.CONTINUOUS, current_value=0.025, start_range=0.0,
-                                end_range=0.09),
+                                end_range=0.09, mutation_chance=0.3),
                 "s_hi": Mutagen(value_type=ValueType.CONTINUOUS, current_value=0.5, start_range=0.1,
-                                end_range=1.0),
+                                end_range=1.0, mutation_chance=0.3),
                 "percent": Mutagen(value_type=ValueType.CONTINUOUS, current_value=0.6, start_range=0.2,
-                                   end_range=0.8)
+                                   end_range=0.8, mutation_chance=0.3)
             },
 
         }
@@ -221,17 +221,17 @@ class DANode(NodeGene):
         if Config.colour_augmentations:
 
             da_submutagens["HSV"] = {
-                    "channel": Mutagen(0, 1, 2, discreet_value=0, mutation_chance=0.20),
+                    "channel": Mutagen(0, 1, 2, discreet_value=0, mutation_chance=0.1),
                     "lo": Mutagen(value_type=ValueType.WHOLE_NUMBERS, current_value=20, start_range=0,
-                                  end_range=29),
+                                  end_range=29, mutation_chance=0.2),
                     "hi": Mutagen(value_type=ValueType.WHOLE_NUMBERS, current_value=50, start_range=30,
-                                  end_range=60)
+                                  end_range=60, mutation_chance=0.2)
                 }
             da_submutagens["Grayscale"] = {
                     "alpha_lo": Mutagen(value_type=ValueType.CONTINUOUS, current_value=0.35, start_range=0.0,
-                                        end_range=0.49),
+                                        end_range=0.49, mutation_chance=0.3),
                     "alpha_hi": Mutagen(value_type=ValueType.CONTINUOUS, current_value=0.75, start_range=0.5,
-                                        end_range=1.0)}
+                                        end_range=1.0, mutation_chance=0.3)}
 
             op = random.choice(list(da_submutagens.keys())) if self.node_type != NodeType.OUTPUT else "No_Operation" # to allow da schemes to start with only 1 op
 
