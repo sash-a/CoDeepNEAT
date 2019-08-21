4ca5b0a32cfe4e04b224ab48a80c379dd381e250
sasha <reallysasha@gmail.com>
2019-08-21 14:11:56 +0200

    Removed old test dir

--- Test/NEAT/test_NEATNode.py
+++
@@ -1,7 +0,0 @@
-from Test.TestingGenomes import nodes
-
-
-def test_midpoint():
-    assert nodes[0].midpoint(nodes[1]) == 0
-    assert nodes[0].midpoint(nodes[3]) == 0.5
-    assert nodes[0].midpoint(nodes[4]) == 1
