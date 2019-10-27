4b55475c0b895dc9d10ce110ce816ec0a2d3a7f3
shane <shaneacton745.sa@gmail.com>
2019-10-27 16:11:42 +0200

    updated skeleton for main program driver

---
+++ src2/main/Main.py
@@ -0,0 +1,12 @@
+from Phenotype import NeuralNetwork
+from main.Generation import Generation
+
+
+def main():
+    pass
+
+def evolve_generation(generation:Generation):
+    pass
+
+def fully_train_NN(model: NeuralNetwork):
+    pass
