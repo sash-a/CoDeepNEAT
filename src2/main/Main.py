b26cc60ee79810c6b296318adf5aabf15174040a
sasha <reallysasha@gmail.com>
2019-11-15 21:45:56 +0200

    Added path appending for running on cluster

--- src2/main/Main.py
+++ src2/main/Main.py
@@ -1,6 +1,16 @@
 from __future__ import annotations
+
 import argparse
 from typing import TYPE_CHECKING
+import os
+import sys
+
+# For importing project files
+dir_path = os.path.dirname(os.path.realpath(__file__))
+dir_path_1 = os.path.split(os.path.split(dir_path)[0])[0]
+dir_path_2 = os.path.split(dir_path)[0]
+sys.path.append(dir_path_1)
+sys.path.append(dir_path_2)
 
 from src2.Configuration import config
 from src2.main.Generation import Generation
