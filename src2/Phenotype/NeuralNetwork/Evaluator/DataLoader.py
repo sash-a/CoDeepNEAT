Fixed unindented method and fixed removed manual seed because it was causing overflow errors

src2/Phenotype/NeuralNetwork/Evaluator/DataLoader.py
@@ -45,10 +45,10 @@ def load_data(composed_transforms: transforms.Compose, split: str) -> DataLoader
 
         # TODO is this the best way to do it, should it be unseeded at the end? should we just manual seed at the start of the program?
         # seeding and unseeding pytorch so that the 'random split' is deterministic
-        initial = torch.initial_seed()
+ #       initial = torch.initial_seed()
         torch.manual_seed(0)
         train, valid = random_split(dataset, [train_size, validation_size])
-        torch.manual_seed(initial)
+  #      torch.manual_seed(initial)
 
         if split == 'train':
             dataset = train

src2/main/Main.py
@@ -139,10 +139,10 @@ def init_operators():
                         + " expected either: centroid | random | best")
     
     def _force_cuda_init_device():
-    import torch
-    for i in range(config.n_gpus):
-        with torch.cuda.device(i):
-            torch.tensor([1.]).cuda()
+        import torch
+        for i in range(config.n_gpus):
+            with torch.cuda.device(i):
+                torch.tensor([1.]).cuda()
 
 
 if __name__ == '__main__':