461dee0b92c9045f9a4a7bc9da61809f6db9a6f1
sasha <reallysasha@gmail.com>
2020-02-02 12:56:46 +0200

    Made get_device method slightly more verbose

--- src2/configuration/configuration.py
+++ src2/configuration/configuration.py
@@ -116,7 +116,7 @@ class Config:
     def get_device(self):
         """Used to obtain the correct device taking into account multiple GPUs"""
         gpu = 'cuda:'
-        gpu_idx = '0' if current_process().name == 'MainProcess' else str(int(current_process().name) % self.n_gpus)
+        gpu_idx = '0' if not current_process().name.isdigit() else str(int(current_process().name) % self.n_gpus)
         # print('extracted device id:', gpu_idx)
         gpu += gpu_idx
         return device('cpu') if self.device == 'cpu' else device(gpu)
