b2200372739c2b58d9126c0f4dbc672603e422f7
shane <shaneacton745.sa@gmail.com>
2020-05-28 02:38:21 +0200

    .

--- test/random_genome_generator.py
+++ test/random_genome_generator.py
@@ -1,14 +1,13 @@
 import random
 from typing import Tuple, Union, Type
 
-from Genotype.NEAT.MutationRecord import MutationRecords
-
-from src.genotype.cdn.genomes.BlueprintGenome import BlueprintGenome
+from src.genotype.cdn.genomes.blueprint_genome import BlueprintGenome
 from src.genotype.cdn.genomes.module_genome import ModuleGenome
 from src.genotype.cdn.nodes.blueprint_node import BlueprintNode
 from src.genotype.cdn.nodes.module_node import ModuleNode
 from src.genotype.neat.connection import Connection
 from src.genotype.neat.genome import Genome
+from src.genotype.neat.mutation_record import MutationRecords
 from src.genotype.neat.node import Node, NodeType
 
 input_node_params = (0, NodeType.INPUT)
@@ -21,7 +20,7 @@ def random_genome(TypeGenome: Union[Type[Genome], Type[ModuleGenome], Type[Bluep
     genome = TypeGenome(
         [TypeNode(input_node_params), TypeNode(2), TypeNode(3), TypeNode(4), TypeNode(5), TypeNode(output_node_params)],
         [Connection(1, 0, 2), Connection(3, 2, 3), Connection(7, 3, 4), Connection(8, 4, 5), Connection(9, 5, 1)])
-    mr = MutationRecords({(0, 1): 0,  # initial mini genome
+    mr: MutationRecords = MutationRecords({(0, 1): 0,  # initial mini genome
                           0: 2,  # add node 2 on connection 0
                           (0, 2): 1,  # add connection for new node
                           (2, 1): 2,  # add connection for new node
@@ -41,10 +40,10 @@ def random_genome(TypeGenome: Union[Type[Genome], Type[ModuleGenome], Type[Bluep
             # Add connection
             from_id = random.randint(0, 6)
             to_id = random.randint(0, 6)
-            if mr._exists((from_id, to_id)):
+            if mr.connection_mut_exists((from_id, to_id)):
                 new_id = mr.connection_mutations[(from_id, to_id)]
             else:
-                new_id = mr.add_mutation((from_id, to_id))
+                new_id = mr.add_conn_mutation((from_id, to_id))
             genome.add_connection(Connection(new_id, from_id, to_id))
         else:
             # Add node
